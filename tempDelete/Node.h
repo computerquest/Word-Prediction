c202560cba9014edd46dd817eaeee89ccf1eea65
Jared Stigter <Jared Stigter@DESKTOP-VR3TR7J>
2017-03-28 16:35:37 -0400

    Preparing to integrate into MasterResource.

--- tempDelete/Node.h
+++ tempDelete/Node.h
@@ -16,12 +16,11 @@ public:
 	}
 
 	Node& addNext(int inputValue) {
-		for (int i = 0; i < nextBranch.size(); i++) {
-			if (nextBranch[i].value == inputValue) {
-				return nextBranch[i];
-			}
+		int pos = -1;
+		if ((pos = binarySearch(nextBranch, 0, nextBranch.size() - 1, inputValue)) != -1) {
+			return nextBranch[pos];
 		}
-		
+
 		insert(inputValue);
 
 		return nextBranch[nextBranch.size()-1];
@@ -55,6 +54,55 @@ private:
 				nextBranch.insert(nextBranch.begin() + a + 1, newRoot);
 				break;
 			}
+		}
+	}
+
+	bool binaryExist(vector<int> input, int target) {
+		if (input.size() == 0) {
+			return 0;
+		}
+		int middle = (input.size()) / 2;
+
+		if (input[middle] == target) {
+			return true;
+		}
+		else if (input.size() == 1 && input[0] != target) {
+			return false;
+		}
+		else if (input[middle] > target) {
+			for (int i = middle; i < input.size(); i++) {
+				input.erase(input.begin() + middle);
+			}
+
+			return binaryExist(input, target);
+		}
+		else if (input[middle] < target) {
+			for (int i = 0; i <= middle; i++) {
+				input.erase(input.begin());
+			}
+
+			return binaryExist(input, target);
+		}
+	}
+
+	int binarySearch(vector<Node> input, int begin, int end, int target)
+	{
+		if (end > input.size() || begin < 0) {
+			return -1;
+		}
+		int middle = (1 + end - begin) / 2;
+
+		if (input[begin + middle].value == target) {
+			return begin + middle;
+		}
+		else if (end - begin == 1 && input[begin].value != target) {
+			return -1;
+		}
+		else if (input[begin + middle].value > target) {
+			return binarySearch(input, begin, begin + (middle - 1), target);
+		}
+		else if (input[middle].value < target) {
+			return binarySearch(input, begin + middle + 1, end, target);
 		}
 	}
 };
